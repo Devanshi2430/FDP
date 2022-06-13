d663f7f8f9dc1d8f419b8cc4f75ca577d9cf958c
Devanshi2430 <devanshipanchal.ce@socet.edu.in>
2022-06-13 17:20:17 +0530

    FDP Day-5

---
+++ Day-5/CSSPractice/JavaScript/Script.js
@@ -0,0 +1,23 @@
+function drawTable(){
+    var row= prompt("Enter row");
+    var column= prompt("Enter column");
+    var html= "<h1> Row: "+ row + "</h1>"; 
+    html += "<h1> Column: "+ column + "</h1>"; 
+    html += "<table border=1>";
+    // document.write(row);
+    // document.write(column);
+    row= parseInt(row);
+    for(var i= 1; i<=row; i++){
+        html += "<tr>";
+        for(var j=1; j<=10; j++){
+            html += "<td> Row:" + i + "; Column:" + j +"</td>";
+        }
+        html += "</tr>";
+    }
+    html += "</table>";
+    document.write(html);
+    console.log(row);
+    console.log(column);
+    // document.getElementById("span_row").innerHTML=row;
+    // document.getElementById("span_col").innerHTML=column;
+}
